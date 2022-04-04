4cbf3cdd1e81987a51482590d90e888045a20da1
FanchinAlberto <6190192@itisrossi.vi.it>
2022-04-04 20:32:42 +0200

    Initial commit

---
+++ UWPtry/Page2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Il modello di elemento Pagina vuota è documentato all'indirizzo https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWPtry
+{
+    /// <summary>
+    /// Pagina vuota che può essere usata autonomamente oppure per l'esplorazione all'interno di un frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void btnShowName_Click(object sender, RoutedEventArgs e)
+        {
+            tbxNameShow.Text = "Alberto Fanchin";
+        }
+
+        private void TryPanel_Click(object sender, RoutedEventArgs e)
+        {
+            myPopUp.IsOpen = true;
+        }
+    }
+}
